4c198b4681b3658fb490282c2cb50f83eec2d0c0
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-09-18 14:37:49 +0800

    added supabase helper functions, and update types

--- src/utils/supabase/types/index.d.ts
+++ src/utils/supabase/types/index.d.ts
@@ -1,5 +1,25 @@
-type IUploadFileToStorageResponse = {
+type IUploadFileToStorageResponse = IUploadFileToStorageResponseSuccess | IUploadFileToStorageResponseError;
+
+type IUploadFileToStorageResponseSuccess = {
+  data: IUploadFileToStorageSuccessResponse;
+  error?: null;
+};
+
+type IUploadFileToStorageResponseError = {
+  data?: null;
+  error: IUploadFileToStorageErrorResponse;
+};
+
+type IUploadFileToStorageSuccessResponse = {
   id: string;
   path: string;
   fullPath: string;
-}
+  publicUrl: string;
+};
+
+type IUploadFileToStorageErrorResponse = {
+  cause?: unknown;
+  message: string;
+  name: string;
+  stack?: string;
+};
