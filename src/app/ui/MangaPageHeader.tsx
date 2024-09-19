af14dc70a4db1c04fcce755b0781dd98657492ab
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-09-19 12:57:20 +0800

    Update MangaPageHeader.tsx
    - change layout

--- src/app/ui/MangaPageHeader.tsx
+++ src/app/ui/MangaPageHeader.tsx
@@ -5,6 +5,7 @@ import { MODEL } from "@/model/model";
 import { getSignedUrlServer } from "@/utils/supabase/helper/server-storage";
 import MuiTypography from "@/components/typography/Typograph";
 import SupabasePublicImage from "@/utils/supabase/components/image/Image";
+import { toBucketPublicUrl } from "@/utils/supabase/helper/image";
 
 type IMangaPageHeader = {
   manga: IMangaTableSelect;
@@ -12,15 +13,16 @@ type IMangaPageHeader = {
 
 const MangaPageHeader: React.FC<IMangaPageHeader> = async (props) => {
   const { manga } = props;
-  const response = await getSignedUrlServer(manga[MODEL.MANGA.THUMBNAIL]);
+
+  const bgUrl = toBucketPublicUrl(manga[MODEL.MANGA.THUMBNAIL]) || "";
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="relative flex justify-center flex-grow">
+    <div className="flex gap-5 flex-wrap">
+      <div className="relative flex justify-center flex-grow max-w-[500px]">
         <div
           className="absolute inset-0 z-0"
           style={{
-            backgroundImage: `url(${response?.data?.signedUrl})`,
+            backgroundImage: `url(${bgUrl})`,
             opacity: 0.5,
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center",
@@ -28,18 +30,22 @@ const MangaPageHeader: React.FC<IMangaPageHeader> = async (props) => {
             filter: "blur(3px)",
           }}
         ></div>
-        <div className="flex z-10 justify-center max-h-[400px] min-h-[400px]">
+        <div className="flex z-10 justify-center max-h-[400px] min-h-[400px] p-5">
           <SupabasePublicImage path={manga[MODEL.MANGA.THUMBNAIL]} alt={manga[MODEL.MANGA.NAME]} />
         </div>
       </div>
-      <MuiTypography fontSize={24}>{manga[MODEL.MANGA.NAME]}</MuiTypography>
-      {manga[MODEL.MANGA.DESCRIPTION] ? (
-        <MuiTypography fontSize={16} variant="body1">
-          {manga[MODEL.MANGA.DESCRIPTION]}
+      <div className="flex flex-grow flex-col gap-5 items-center max-w-[500px] mx-auto">
+        <MuiTypography fontSize={32} textAlign={"center"}>
+          {manga[MODEL.MANGA.NAME]}
         </MuiTypography>
-      ) : (
-        <em>no description</em>
-      )}
+        {manga[MODEL.MANGA.DESCRIPTION] ? (
+          <MuiTypography fontSize={16} variant="body1">
+            {manga[MODEL.MANGA.DESCRIPTION]}
+          </MuiTypography>
+        ) : (
+          <em>no description</em>
+        )}
+      </div>
     </div>
   );
 };
