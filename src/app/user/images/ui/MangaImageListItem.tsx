d1c30c5ba309560c65b2a885cf686321315b5bb0
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-21 22:58:56 +0800

    added placeholder 404 image

--- src/app/user/images/ui/MangaImageListItem.tsx
+++ src/app/user/images/ui/MangaImageListItem.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import MuiImageList, { MuiImageListItem, MuiImageListItemBar } from "@/components/image/Image";
-import React, { useCallback, useMemo, useState } from "react";
+import { MuiImageListItem, MuiImageListItemBar } from "@/components/image/Image";
+import React, { useMemo } from "react";
 import { toBucketPublicUrl } from "@/utils/supabase/helper/image";
 import { HiPhoto } from "react-icons/hi2";
 import MuiIconButton from "@/components/icon-button/IconButton";
-import { IMangaImageTableSelect, IMangaTableSelect } from "@/utils/drizzle/schema";
+import { IMangaTableSelect } from "@/utils/drizzle/schema";
 import { MODEL } from "@/model/model";
+import Image from "next/image";
 
 type IMangaImageListItem = {
   index?: number;
@@ -28,11 +29,15 @@ const MangaImageListItem: React.FC<IMangaImageListItem> = (props) => {
   //   return manga[MODEL.MANGA.THUMBNAIL] === image[MODEL.MANGA_IMAGE.PATH];
   // }, [image, manga, isLoading]);
 
+  const srcPath = useMemo(() => {
+    return manga[MODEL.MANGA.THUMBNAIL] ? `${toBucketPublicUrl(manga[MODEL.MANGA.THUMBNAIL])}` : "/images/404.jpg";
+  }, [manga]);
+
   return (
     <MuiImageListItem key={manga.id}>
       <img
-        srcSet={`${toBucketPublicUrl(manga[MODEL.MANGA.THUMBNAIL])}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-        src={`${toBucketPublicUrl(manga[MODEL.MANGA.THUMBNAIL])}?w=164&h=164&fit=crop&auto=format`}
+        srcSet={`${srcPath}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+        src={`${srcPath}?w=164&h=164&fit=crop&auto=format`}
         alt={manga[MODEL.MANGA.NAME]}
         loading="lazy"
       />
