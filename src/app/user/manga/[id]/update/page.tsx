7628c2f6b7a50675bdfe6d955d9e47765aa2e68d
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-22 22:17:23 +0800

    refactor, change update manga components

--- src/app/user/manga/[id]/update/page.tsx
+++ src/app/user/manga/[id]/update/page.tsx
@@ -4,14 +4,12 @@ import PageBreadCrumbs from "@/components/custom/PageBreadCrumbs";
 import MuiPaper from "@/components/paper/Paper";
 import Dashboard from "@/components/ui/Dashboard";
 import React from "react";
-import MangaPageHeader from "@/app/ui/MangaPageHeader";
 import MangaUploadImage from "@/app/ui/MangaUploadImage";
-import MuiTextField from "@/components/text-field/TextField";
-import { updateMangaFormAction } from "@/app/action/manga";
-import MuiButton from "@/components/button/Button";
-import { TEXT } from "@/components/helper/field";
 import MangaBanner from "@/app/ui/MangaBanner";
 import MangaImageList from "@/app/ui/MangaImageList";
+import MangaForm from "../../ui/form/MangaForm";
+import UpdateMangaForm from "../../ui/form/UpdateMangaForm";
+import { MODEL } from "@/model/model";
 
 const UpdateMangaPage: React.FC<INextPage> = async (props) => {
   const { params } = props;
@@ -25,34 +23,14 @@ const UpdateMangaPage: React.FC<INextPage> = async (props) => {
   return (
     <Dashboard>
       <MuiPaper>
-        <PageBreadCrumbs route="/user" pathNames={[manga.data.name]} />
+        <PageBreadCrumbs route="/user" pathNames={[manga.data[MODEL.MANGA.NAME]]} />
       </MuiPaper>
       <div className="flex flex-grow-[2] gap-4 flex-wrap">
         <MuiPaper className="flex flex-col flex-grow-[2] min-h-[320px] p-4 gap-6" elevation={2} color="primary">
           <MangaBanner manga={manga.data} />
         </MuiPaper>
         <MuiPaper className="flex flex-col flex-grow-[2] min-h-[320px] p-4 gap-6" elevation={2} color="primary">
-          <form action={updateMangaFormAction} className="flex flex-col gap-6">
-            <div className="grid grid-cols-1 gap-4 ">
-              <input className="hidden" name="id" defaultValue={manga.data.id} />
-              <MuiTextField
-                required
-                label={"Title"}
-                name="name"
-                defaultValue={manga.data.name}
-                maxLength={TEXT.MAX.LONG}
-              />
-              <MuiTextField
-                label={"Description"}
-                name="description"
-                defaultValue={manga.data.description}
-                maxLength={TEXT.MAX.DESCRIPTION}
-                multiline
-                rows={20}
-              />
-            </div>
-            <MuiButton type="submit">Update</MuiButton>
-          </form>
+          <UpdateMangaForm manga={manga.data} />
         </MuiPaper>
       </div>
       <div className="flex flex-grow-[2] gap-4 flex-wrap">
