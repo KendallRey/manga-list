5f0bae38ed70545654a5a41bea53518dcecb879e
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-22 22:16:22 +0800

    refactor, create manga form component

---
+++ src/app/user/manga/ui/form/MangaForm.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { FormCheckbox } from "@/components/checkbox/Checkbox";
+import { TEXT } from "@/components/helper/field";
+import MuiTextField from "@/components/text-field/TextField";
+import { MODEL } from "@/model/model";
+import { editMangaForm } from "@/redux/features/manga/mangaFormSlice";
+import { InputRecord } from "@/redux/helper/input";
+import useReduxForm from "@/redux/hooks/useReduxForm";
+import { useAppSelector } from "@/redux/services/hooks";
+import React, { useCallback } from "react";
+
+type IMangaForm = {
+  children?: React.ReactNode;
+  isLoading?: boolean;
+} & React.ComponentProps<"form">;
+
+const MangaForm: React.FC<IMangaForm> = (props) => {
+  const { children, isLoading, ...otherProps } = props;
+
+  const { error, ...form } = useAppSelector((state) => state.mangaFormSlice);
+  const { onChangeForm } = useReduxForm();
+
+  const onChange = useCallback((e: InputRecord) => {
+    onChangeForm(e, editMangaForm);
+  }, []);
+
+  return (
+    <>
+      <form className="flex flex-col gap-6" {...otherProps}>
+        <div className="grid grid-cols-1 gap-4 ">
+          <MuiTextField
+            required
+            label={"Title"}
+            name={MODEL.MANGA.NAME}
+            value={form[MODEL.MANGA.NAME] ?? ""}
+            maxLength={TEXT.MAX.LONG}
+            errorText={error?.[MODEL.MANGA.NAME]}
+            onChange={onChange}
+            disabled={isLoading}
+          />
+          <MuiTextField
+            label={"Description"}
+            name={MODEL.MANGA.DESCRIPTION}
+            value={form[MODEL.MANGA.DESCRIPTION] ?? ""}
+            maxLength={TEXT.MAX.DESCRIPTION}
+            onChange={onChange}
+            disabled={isLoading}
+            multiline
+            rows={20}
+          />
+        </div>
+        <div className="d-flex gap-2 items-center flex-wrap">
+          <FormCheckbox
+            label="Hide"
+            name={MODEL.MANGA.HIDE}
+            checked={form[MODEL.MANGA.HIDE] ?? false}
+            onChange={onChange}
+            disabled={isLoading}
+          />
+          <FormCheckbox
+            label="Spicy"
+            name={MODEL.MANGA.SPICY}
+            checked={form[MODEL.MANGA.SPICY] ?? false}
+            onChange={onChange}
+            disabled={isLoading}
+          />
+          <FormCheckbox
+            label="Danger"
+            name={MODEL.MANGA.DANGER}
+            checked={form[MODEL.MANGA.DANGER] ?? false}
+            onChange={onChange}
+            disabled={isLoading}
+          />
+        </div>
+        {children}
+      </form>
+    </>
+  );
+};
+
+export default MangaForm;
