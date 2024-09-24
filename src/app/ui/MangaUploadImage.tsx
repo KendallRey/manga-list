f406dd98cfae3e3f7cc43b9f6143fdbdbe8cd586
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-24 22:41:13 +0800

    fix multiple image upload function

--- src/app/ui/MangaUploadImage.tsx
+++ src/app/ui/MangaUploadImage.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { addMangaImageAction, addMangaImagesAction } from "@/app/action/manga";
+import { addMangaImagesAction } from "@/app/action/manga";
 import { IMangaTableSelect } from "@/utils/drizzle/schema";
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 import UploadFile, { IImageToUpload } from "../../components/custom/UploadFile";
 import { uploadMangaImageToStorage } from "@/app/api/storage/upload";
 import { displaySnackbar } from "@/components/helper/notistack";
 import { cleanString } from "@/components/helper/string";
-import { FormCheckbox } from "@/components/checkbox/Checkbox";
 
 type IMangaUploadImage = {
   manga: IMangaTableSelect;
@@ -16,40 +15,21 @@ type IMangaUploadImage = {
 const MangaUploadImage: React.FC<IMangaUploadImage> = (props) => {
   const { manga } = props;
 
-  const [makeThumbnail, setMakeThumbnail] = useState(true);
-
-  const uploadFn = useCallback(
-    async (file: File) => {
-      const { data, error } = await uploadMangaImageToStorage(file, cleanString(manga.name));
-      if (error) {
-        displaySnackbar({ action: "upload", status: "error", variant: "error" });
-        return { error: error.message };
-      }
-      const payload = { manga: manga, imageData: data, setAsThumbnail: makeThumbnail };
-      const response = await addMangaImageAction({ payload });
-      if (response.error) {
-        displaySnackbar({ action: "upload", status: "error", variant: "error" });
-        return { error: response.error };
-      }
-      displaySnackbar({ action: "upload", status: "success", variant: "success" });
-      return { data: response.data };
-    },
-    [manga, makeThumbnail],
-  );
-
   const uploadsFn = useCallback(
     async (images: IImageToUpload[]) => {
       const ids: string[] = [];
       const successImages: IUploadFileToStorageSuccessResponse[] = [];
-
-      images.forEach(async (image) => {
+      let thumbNailId = "";
+      for (const image of images) {
         const { data, error } = await uploadMangaImageToStorage(image.file, cleanString(manga.name));
-        if (error) return;
-        ids.push(image.key);
-        successImages.push(data);
-      });
+        if (!error) {
+          ids.push(image.key);
+          successImages.push(data);
+          if (image.setAsCover) thumbNailId = data.id;
+        }
+      }
 
-      const payload = { manga: manga, imagesData: successImages, setAsThumbnail: makeThumbnail };
+      const payload = { manga: manga, imagesData: successImages, imageThumbnailId: thumbNailId };
       const response = await addMangaImagesAction({ payload });
       if (response.error) {
         displaySnackbar({ action: "upload", status: "error", variant: "error" });
@@ -58,21 +38,12 @@ const MangaUploadImage: React.FC<IMangaUploadImage> = (props) => {
       }
       return ids;
     },
-    [manga, makeThumbnail],
-  );
-
-  const onChangeSetAsCover = useCallback(
-    (e: RCE<HTMLInputElement>) => {
-      const { checked } = e.target;
-      setMakeThumbnail(checked);
-    },
-    [setMakeThumbnail],
+    [manga],
   );
 
   return (
     <div>
-      <FormCheckbox label="Set as Cover" checked={makeThumbnail} onChange={onChangeSetAsCover} />
-      <UploadFile uploadFn={uploadFn} uploadsFn={uploadsFn} actionText="Upload Image" />
+      <UploadFile uploadsFn={uploadsFn} actionText="Upload Image" />
     </div>
   );
 };
