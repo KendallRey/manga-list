facba5d74d1557999a4798bd51c78844c8c83508
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-30 23:22:34 +0800

    update create manga list component

--- src/app/user/manga/ui/CreateMangaList.tsx
+++ src/app/user/manga/ui/CreateMangaList.tsx
@@ -3,18 +3,21 @@ import MuiButton from "@/components/button/Button";
 import MuiTypography from "@/components/typography/Typograph";
 import PageTitle from "@/components/custom/PageTitle";
 import { createMangaListAction } from "@/app/action/manga";
+import MuiPaper from "@/components/paper/Paper";
 
 const CreateMangaList = () => {
   return (
-    <div className="flex flex-col gap-2">
-      <PageTitle>Manga List</PageTitle>
-      <MuiTypography variant="h6">Start listing your manga here</MuiTypography>
-      <form className="py-6">
-        <MuiButton formAction={createMangaListAction} type="submit">
-          Get started
-        </MuiButton>
-      </form>
-    </div>
+    <MuiPaper className="flex-grow p-4 flex">
+      <div className="flex flex-col gap-2">
+        <PageTitle>Manga List</PageTitle>
+        <MuiTypography variant="h6">Start listing your manga here</MuiTypography>
+        <form className="py-6">
+          <MuiButton formAction={createMangaListAction} type="submit">
+            Get started
+          </MuiButton>
+        </form>
+      </div>
+    </MuiPaper>
   );
 };
 
