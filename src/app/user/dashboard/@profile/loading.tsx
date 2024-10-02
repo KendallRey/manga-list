df9abe418b905ca6750a674bdd06530d3e9590ca
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-10-02 12:50:54 +0800

    update initial profile slot at dashboard

--- src/app/user/dashboard/@profile/loading.tsx
+++ src/app/user/dashboard/@profile/loading.tsx
@@ -1,13 +1,13 @@
-import MuiPaper from '@/components/paper/Paper'
-import MuiSkeleton from '@/components/skeleton/Skeleton'
-import React from 'react'
+import DisplayUserProfileSkeleton from "@/app/ui/user-profile/DisplayUserProfileSkeleton";
+import MuiPaper from "@/components/paper/Paper";
+import React from "react";
 
 const DasboardProfileLoading = () => {
   return (
     <MuiPaper className="flex items-center justify-center min-h-[240px] gap-6 p-4">
-      <MuiSkeleton width={140} height={140} variant='circular'/>
+      <DisplayUserProfileSkeleton />
     </MuiPaper>
-  )
-}
+  );
+};
 
-export default DasboardProfileLoading
+export default DasboardProfileLoading;
--- src/app/user/dashboard/@profile/page.tsx
+++ src/app/user/dashboard/@profile/page.tsx
@@ -1,11 +1,24 @@
-import MuiAvatar from "@/components/avatar/Avatar";
+import { GetUserProfiles } from "@/app/api/user-profile/user-profile-api";
+import CreateUserProfile from "@/app/ui/user-profile/CreateUserProfile";
+import DisplayUserProfile from "@/app/ui/user-profile/DisplayUserProfile";
 import MuiPaper from "@/components/paper/Paper";
 import React from "react";
 
-const DashboardProfile = () => {
+const DashboardProfile: React.FC<INextPage> = async (props) => {
+  const userProfilesResponse = await GetUserProfiles({});
+
+  if (!userProfilesResponse.status) {
+    return <MuiPaper></MuiPaper>;
+  }
+  if (!userProfilesResponse.data.length) {
+    return <CreateUserProfile />;
+  }
+
+  const userProfile = userProfilesResponse.data[0];
+
   return (
-    <MuiPaper className="flex items-center justify-center min-h-[240px] gap-6 p-4">
-      <MuiAvatar sx={{ width: 140, height: 140 }} />
+    <MuiPaper className="flex items-center flex-wrap justify-center min-h-[240px] gap-6 p-4">
+      <DisplayUserProfile userProfile={userProfile} />
     </MuiPaper>
   );
 };
