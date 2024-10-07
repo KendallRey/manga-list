18f8c311a9d069d2fb1eddd1461e2aa676c3eefb
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-10-07 12:42:28 +0800

    added custom component DisplayList

---
+++ src/components/helper-components/DisplayList.tsx
@@ -0,0 +1,34 @@
+
+import { CircularProgress } from '@mui/material';
+import React from 'react';
+import MuiTypography from '../typography/Typograph';
+import { Each } from './TableList';
+
+type IDisplayList<T> = {
+  data?: T[] | null;
+  render: (item: T, index: number) => React.ReactNode;
+  isLoading?: boolean;
+  isError?: boolean;
+  errorText?: string;
+  emptyText?: string;
+  className?: string;
+};
+
+const DisplayList = <T,>(props: IDisplayList<T>) => {
+  const { isError, isLoading, errorText, className, emptyText, data, render } = props;
+
+  return (
+    <>
+      {isLoading && <CircularProgress className={`mx-auto ${className ?? ''}`} />}
+      {isError && (
+        <MuiTypography className={`text-destructive-500 mx-auto text-center ${className ?? ''}`} component={'strong'}>
+          {errorText ?? 'Something went wrong.'}
+        </MuiTypography>
+      )}
+      {data && !data?.length ? <MuiTypography component={'em'} className={className}>{emptyText ?? 'No record/s found'}</MuiTypography> : <></>}
+      <Each data={data ?? []} render={render} />
+    </>
+  );
+};
+
+export default DisplayList;
