b755710f4e806ecf44c5cd308e0f130d3e6e7794
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-13 01:03:27 +0800

    use Profile Update Component to Profile Page

--- src/app/user/profile/update/page.tsx
+++ src/app/user/profile/update/page.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import UserProfile from "../ui/UserProfile";
 import UpdateUserProfileForm from "@/app/ui/user-profile/UpdateUserProfileForm";
 import PageTitle from "@/components/custom/PageTitle";
+import UploadImageFile from "@/components/custom/UploadImageFile";
+import UpdateUserProfile from "../ui/UpdateUserProfile";
 
 const ProfileUpdatePage = () => {
   return (
@@ -11,8 +13,8 @@ const ProfileUpdatePage = () => {
       <MuiPaper className="flex flex-col items-center p-4" elevation={2}>
         <UserProfile />
       </MuiPaper>
-      <MuiPaper className="p-4 bg-primary-500" elevation={2}>
-        <UpdateUserProfileForm />
+      <MuiPaper className="p-4 flex flex-col gap-6 bg-primary-500" elevation={2}>
+        <UpdateUserProfile />
       </MuiPaper>
       <MuiPaper className="flex-grow p-4" elevation={2}>
         <PageTitle>Profile Images</PageTitle>
