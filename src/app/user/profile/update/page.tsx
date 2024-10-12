19bfef33a2f58495ef5a05725aa613a21063b84c
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-12 23:20:43 +0800

    update Profile component

--- src/app/user/profile/update/page.tsx
+++ src/app/user/profile/update/page.tsx
@@ -3,6 +3,7 @@ import Dashboard from "@/components/ui/Dashboard";
 import React from "react";
 import UserProfile from "../ui/UserProfile";
 import UpdateUserProfileForm from "@/app/ui/user-profile/UpdateUserProfileForm";
+import PageTitle from "@/components/custom/PageTitle";
 
 const ProfileUpdatePage = () => {
   return (
@@ -10,11 +11,11 @@ const ProfileUpdatePage = () => {
       <MuiPaper className="flex flex-col items-center p-4" elevation={2}>
         <UserProfile />
       </MuiPaper>
-      <MuiPaper className="flex-grow p-4 bg-primary-500" elevation={2}>
+      <MuiPaper className="p-4 bg-primary-500" elevation={2}>
         <UpdateUserProfileForm />
       </MuiPaper>
       <MuiPaper className="flex-grow p-4" elevation={2}>
-        Images
+        <PageTitle>Profile Images</PageTitle>
       </MuiPaper>
     </Dashboard>
   );
