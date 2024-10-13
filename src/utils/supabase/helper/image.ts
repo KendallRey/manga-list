46537d0d2780bddcc577d9c7732fc2074b2afa0f
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-13 20:59:41 +0800

    added to public profile image url helper func

--- src/utils/supabase/helper/image.ts
+++ src/utils/supabase/helper/image.ts
@@ -6,6 +6,12 @@ export const toBucketPublicMangaUrl = (fullPath: string | null, width?: number, 
   return `${SUPABASE.BUCKET_MANGA.PUBLIC_LOADER_URL}/${fullPath}?width=${width || WIDTH}&quality=${quality || QUALITY}`;
 };
 
+export const toBucketPublicProfileUrl = (fullPath: string | null, width?: number, quality?: number) => {
+  if (!fullPath) return;
+  const { WIDTH, QUALITY } = SUPABASE.BUCKET_PROFILE.IMAGE;
+  return `${SUPABASE.BUCKET_PROFILE.PUBLIC_LOADER_URL}/${fullPath}?width=${width || WIDTH}&quality=${quality || QUALITY}`;
+};
+
 export const toBucketPublicBlurUrl = (fullPath?: string | null) => {
   if (!fullPath) return;
   return `${SUPABASE.BUCKET_MANGA.PUBLIC_LOADER_URL}/${fullPath}?width=${120}&quality=${20}`;
