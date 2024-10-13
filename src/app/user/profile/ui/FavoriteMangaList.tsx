d200f296431b5d75dd115ce200c553756c5278db
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-10-13 21:53:39 +0800

    Update FavoriteMangaList.tsx

--- src/app/user/profile/ui/FavoriteMangaList.tsx
+++ src/app/user/profile/ui/FavoriteMangaList.tsx
@@ -36,12 +36,10 @@ const FavoriteMangaList: React.FC<IFavoriteMangaList> = async (props) => {
     return <ErrorPage />;
   }
 
-  console.log("test", mangaListResponse);
-
   return (
     <>
       <PageTitle>Favorites</PageTitle>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         <DisplayList
           data={mangaListResponse.data.results}
           render={(item) => <MangaCard key={item.id} manga={item} sx={{ height: 500 }} />}
