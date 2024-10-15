1805e211a829d7e48cbc830303aeb8420adbe651
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-10-15 16:09:04 +0800

    added Skeleton for Manga components

---
+++ src/app/ui/MangaPageHeaderSkeleton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import MangaBannerSkeleton from "./MangaBannerSkeleton";
+import MuiStack from "@/components/stack/Stack";
+import MuiSkeleton from "@/components/skeleton/Skeleton";
+
+const MangaPageHeaderSkeleton = () => {
+  return (
+    <div className="flex gap-5 flex-wrap">
+      <MangaBannerSkeleton />
+      <div className="flex flex-grow flex-col gap-5 items-center max-w-[500px] mx-auto">
+        <MuiStack marginTop={4} gap={1.3}>
+          <MuiSkeleton variant="text" height={20} width={Math.max(Math.random(), 0.5) * 450} />
+          <MuiSkeleton variant="text" height={13} width={60} />
+        </MuiStack>
+        <MuiSkeleton variant="text" height={13} width={Math.random() * 550} className="mt-8 mb-7" />
+      </div>
+    </div>
+  );
+};
+
+export default MangaPageHeaderSkeleton;
