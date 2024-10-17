4b053ea10812f2afbd7e057788c621aca0ebb2ee
KendallRey <107897863+KendallRey@users.noreply.github.com>
2024-10-17 12:39:11 +0800

    added PreviewMangaDialog to manga list page

--- src/app/user/manga/page.tsx
+++ src/app/user/manga/page.tsx
@@ -9,6 +9,7 @@ import { getSearchParams } from "@/app/api/helper/apiHelper";
 import MangaListFilter from "./ui/MangaListFilterSorting";
 import MangaSearchAdd from "../../ui/manga/MangaSearchAdd";
 import MangaList from "@/app/ui/manga/MangaList";
+import PreviewMangaDialog from "@/app/ui/manga/PreviewMangaDialog";
 
 const MangaPage: React.FC<INextPage> = async (props) => {
   const { searchParams } = props;
@@ -37,16 +38,19 @@ const MangaPage: React.FC<INextPage> = async (props) => {
   }
 
   return (
-    <Dashboard>
-      <MuiPaper className="p-4" elevation={2} color="primary">
-        <MangaSearchAdd listId={mangaList.id} searchParams={searchParams} />
-      </MuiPaper>
+    <>
+      <Dashboard>
+        <MuiPaper className="p-4" elevation={2} color="primary">
+          <MangaSearchAdd listId={mangaList.id} searchParams={searchParams} />
+        </MuiPaper>
 
-      <MuiPaper className="flex flex-col flex-grow-[2] gap-1 min-h-[320px] p-4" elevation={2} color="primary">
-        <MangaListFilter searchParams={searchParams} />
-        <MangaList />
-      </MuiPaper>
-    </Dashboard>
+        <MuiPaper className="flex flex-col flex-grow-[2] gap-1 min-h-[320px] p-4" elevation={2} color="primary">
+          <MangaListFilter searchParams={searchParams} />
+          <MangaList />
+        </MuiPaper>
+      </Dashboard>
+      <PreviewMangaDialog />
+    </>
   );
 };
 
