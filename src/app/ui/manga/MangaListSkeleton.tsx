e63ec6fb54e1e86191ff19b36b1d966d1872691d
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-04-22 23:03:16 +0800

    update MangaListSkelecton, make item width responsive

--- src/app/ui/manga/MangaListSkeleton.tsx
+++ src/app/ui/manga/MangaListSkeleton.tsx
@@ -1,12 +1,28 @@
 import ComponentList from "@/components/helper-components/ComponentList";
-import MuiList from "@/components/list/List";
+import MuiList, { MuiListItem, MuiListItemAvatar, MuiListItemText } from "@/components/list/List";
 import React from "react";
-import MangaListItemSkeleton from "./MangaListItemSkeleton";
+import { MathRandom } from "@/components/helper/math";
+import MuiSkeleton from "@/components/skeleton/Skeleton";
+import MuiStack from "@/components/stack/Stack";
 
 const MangaListSkeleton = () => {
   return (
     <MuiList>
-      <ComponentList count={10} render={(id) => <MangaListItemSkeleton key={id} />} />
+      <ComponentList
+        count={10}
+        render={(i) => (
+          <MuiListItem key={i} className="border-b">
+            <MuiListItemAvatar>
+              <MuiSkeleton width={50} height={50} />
+            </MuiListItemAvatar>
+            <MuiListItemText
+              disableTypography
+              primary={<MuiSkeleton style={{ maxWidth: MathRandom(500, 0.3) }} />}
+              secondary={<MuiStack direction={"row"} gap={1}></MuiStack>}
+            />
+          </MuiListItem>
+        )}
+      />
     </MuiList>
   );
 };
