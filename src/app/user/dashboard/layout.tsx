d3aac9b9f8d2089895cb36721c165a33af3ec31c
UVMapping <107897863+KendallRey@users.noreply.github.com>
2025-04-22 23:49:07 +0800

    update Dashboard layout type, fix Vercel Build Error

--- src/app/user/dashboard/layout.tsx
+++ src/app/user/dashboard/layout.tsx
@@ -1,17 +1,17 @@
 import Dashboard from "@/components/ui/Dashboard";
 import React from "react";
 
-type IDashboardLayoutProps = {
-  children: React.ReactNode | undefined;
-  manga: React.ReactNode | undefined;
-  profile: React.ReactNode | undefined;
-  randomList: React.ReactNode | undefined;
-  preview: React.ReactNode | undefined;
-};
-
-const DashboardLayout: React.FC<IDashboardLayoutProps> = (props) => {
-  const { preview, manga, profile, randomList } = props;
-
+const DashboardLayout = ({
+  preview,
+  manga,
+  profile,
+  randomList,
+}: {
+  manga: React.ReactNode;
+  profile: React.ReactNode;
+  randomList: React.ReactNode;
+  preview: React.ReactNode;
+}) => {
   return (
     <Dashboard>
       {preview}
